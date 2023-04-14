d7c5de622d6de1617430477c694e4f879e4a1f59
AliyenSha <zihadshai701@gmail.com>
2023-04-14 06:54:53 +0600

    More about data types learn

--- src/CsharpPractice/02DataTypes/Program.cs
+++ src/CsharpPractice/02DataTypes/Program.cs
@@ -1,36 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//boolean type
-bool isTrue = false;
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+////boolean type
+//bool isTrue = false;
 
-//integer type
-int age;
-age = 22;
+////integer type
+//int age;
+//age = 22;
 
-Console.WriteLine(age);
+//Console.WriteLine(age);
 
-//Floating point 
-float r = 3.00f;
+////Floating point 
+//float r = 3.00f;
 
-//doouble type 
-double pi = 3.141592653589793238;
+////doouble type 
+//double pi = 3.141592653589793238;
 
-//decimal type
-decimal dec = 0.12542654224235m;
+////decimal type
+//decimal dec = 0.12542654224235m;
 
-//charecter type
-char c = 'a';
+////charecter type
+//char c = 'a';
 
-//string type
-string greetings = "Hi everyone!";
+////string type
+//string greetings = "Hi everyone!";
 
-//object type: The base of all other type 
-object obj = "Hello world";
+////object type: The base of all other type 
+//object obj = "Hello world";
 
-//Nullable types
-int? nulllableNUm = null;
+////Nullable types
+//int? nulllableNUm = null;
 
-//To assign a value to a nullable type, you can use the null coalescing operator "??"
-//Example: 
-int num = nulllableNUm ?? 42;
-Console.WriteLine(num);
+////To assign a value to a nullable type, you can use the null coalescing operator "??"
+////Example: 
+//int num = nulllableNUm ?? 42;
+//Console.WriteLine(num);
+
+
+//Task 01 for data type practice ---->>
+/*
+ var input = Console.ReadLine();
+int number;
+bool isNumeric = int.TryParse(input, out number);
+
+if (isNumeric)
+{
+    
+    Console.WriteLine(convert.ToDouble(input));
+}
+else
+{
+    Console.WriteLine("Enter a valid number!");
+}
+
+//Format exception!
+//Updated one-->
+var input = Console.ReadLine();
+int number;
+
+if (int.TryParse(input, out number))
+{
+    Console.WriteLine(convert.ToDouble(input));
+}
+else
+{
+    Console.WriteLine("Enter a valid number!");
+}
+*/
+
+//Task02 ========>
+
+Console.WriteLine("Enter a decimal number: ");
+decimal DecInput = decimal.Parse(Console.ReadLine());
+
+string BinaryOutput = "";
+
+BinaryOutput += Convert.ToString((int)DecInput, toBase: 2);
+
+if(DecInput % 1 != 0)
+{
+    decimal fractionalPart = DecInput % 1;
+    BinaryOutput += ".";
+
+    for(int i =0; i<10; i++)
+    {
+        fractionalPart *= 2;
+        BinaryOutput += ((int)fractionalPart).ToString();
+        fractionalPart -= (int)fractionalPart;
+    }
+}
+Console.WriteLine(BinaryOutput);
---
+++ src/CsharpPractice/03Array/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Array practice process on");
+
+//single dimentions array!
+int[] ages = new int[10];
+
+ages[0] = 22;
+ages[1] = 23;
+ages[2] = 24;
+ages[3] = 25;
+ages[4] = 26;
+ages[5] = 27;
+ages[6] = 28;
+ages[7] = 29;
+ages[8] = 30;
+ages[9] = 31;
+//iteration proccess of array==>
+foreach (var age in ages)
+{
+    Console.WriteLine(age);
+}
+
+//another way to declare single dimentions array
+int[] reg = new int[10] {10, 12,13,14,15,16,17,18,19,20};
+
+Console.WriteLine(reg[3]);
+
+//Multi dimentional array
+int[,] score = new int[2,4];
+score[0,0] = 33;
+score[0,1] = 42;
+score[0,2] = 43;
+
+foreach(var s in score)
+{
+    Console.WriteLine($"Socores are: {s}");
+}
+
+//Jagged array
+int[][] scores = new int[3][];
+scores[0] = new int[5];
+scores[1] = new int[5];
+scores[2] = new int[5];
+
+scores[1][3] = 5;
+
+Console.WriteLine($"Jagged array: {scores[1][3]}");
