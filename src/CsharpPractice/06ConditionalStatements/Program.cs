5525231027b33498111780200279c12dcefea484
AliyenSha <zihadshai701@gmail.com>
2023-04-16 07:52:44 +0600

    Some exercise on DataTypes practiced and conditionaal statements are process on

--- src/CsharpPractice/06ConditionalStatements/Program.cs
+++ src/CsharpPractice/06ConditionalStatements/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int x = 5;
+if(x > 10)
+{
+    Console.WriteLine("X is Greater than 10!");
+}
+else
+{
+    Console.WriteLine("X is less than 10!");
+}
