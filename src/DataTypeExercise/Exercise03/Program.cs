1b6adfdc421934fbe1aa7b9a4a33b553b6fdcf18
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:18:39 +0600

    Exercise 3 codes

---
+++ src/DataTypeExercise/Exercise03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Create a program that asks the user to enter a number and then displays whether it is even or odd.
+
+Console.WriteLine("Enter a number: ");
+int input =  int.Parse(Console.ReadLine());
+
+if(input % 2 == 0)
+{
+    Console.WriteLine($"{input} is a Even number!");
+}
+else
+{
+    Console.WriteLine($"{input} is a Odd number!");
+}
