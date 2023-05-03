55558c8d7020d3a7ba478549cae1c2bde848eff5
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:20:26 +0600

    Advance Exercise 11 codes

---
+++ src/DataTypeExercise/ExerciseAdv11/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a decimal number: ");
+decimal input  = decimal.Parse(Console.ReadLine());
+
+int intPart = (int)input;
+string Binary = Convert.ToString((byte)input, toBase: 2);
+
+if(input % 1 != 0)
+{
+
+}
