532d51bde2d7119d9749af56c12b5f32472a9989
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:19:38 +0600

    Exercise 9 codes

---
+++ src/DataTypeExercise/Exercise09/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your birth year: ");
+int bYear = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your age is: {DateTime.Now.Year - bYear}");
