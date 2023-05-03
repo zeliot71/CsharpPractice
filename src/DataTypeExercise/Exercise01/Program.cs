be1869d0152d5df423205cc3ef55555715ce250c
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:17:38 +0600

    Exercise 1 codes

---
+++ src/DataTypeExercise/Exercise01/Program.cs
@@ -0,0 +1,10 @@
+﻿//Create a program that asks the user to enter their name and age.
+//Then, display a message that says "Hello [name], you are [age] years old!".
+
+Console.WriteLine("Enter your name and Age: ");
+var input = Console.ReadLine().Split(' ');
+
+string name = input[0];
+uint age = uint.Parse(input[1]);
+
+Console.WriteLine($"Hello {name}, you are {age} years old!");
