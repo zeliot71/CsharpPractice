92222481a1673ae02d36fc84b757d20e5538cf0e
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:19:20 +0600

    Exercise 7 codes

---
+++ src/DataTypeExercise/Exercise07/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"The sum of first n'th number is: {n*(n+1)/2}");
