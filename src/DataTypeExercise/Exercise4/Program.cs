5e529dd55e849b4d49a92543a3835d044f95a79b
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:19:00 +0600

    Exercise 4 codes

---
+++ src/DataTypeExercise/Exercise4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a temperature in Farenhite!");
+int f = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The temperature is {(f- 32)* 0.56} degree celsius!");
