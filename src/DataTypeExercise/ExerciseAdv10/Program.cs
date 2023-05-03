ddfbda4b8f4bc46903a0ee483cd15d05386ecd36
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:20:03 +0600

    Advance Exercise 1 codes

---
+++ src/DataTypeExercise/ExerciseAdv10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Advance 01
+
+Console.WriteLine("Enter any string: ");
+var input = Console.ReadLine();
+
+if(double.TryParse(input, out var value))
+{
+    Console.WriteLine($"The parsed double value is: {input}");
+}
+else
+{
+    Console.WriteLine("The input is not a valid input!");
+}
+
