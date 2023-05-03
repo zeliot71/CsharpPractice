529cd20d6feeccf3d20d75cab7aaf2de1a188490
AliyenSha <zihadshai701@gmail.com>
2023-05-03 22:18:10 +0600

    Exercise 2 codes

---
+++ src/DataTypeExercise/Exercise02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Write a program that calculates the area of a rectangle.
+// Ask the user to enter the length and width of the rectangle and then display the result
+
+Console.WriteLine("Enter the length and width of rectangle: ");
+var input = Console.ReadLine().Split(' ');
+uint length = uint.Parse(input[0]);
+uint width = uint.Parse(input[1]);
+Console.WriteLine($"The area of rectangle is: {length * width} sqrft");
