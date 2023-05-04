a232c4bf2732074b79094c68c918f6740525d9b7
AliyenSha <zihadshai701@gmail.com>
2023-05-04 09:34:34 +0600

    Exercise1 Codes

---
+++ src/ExerciseForArray/Exercise01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Write a program that creates an array of integers with the values 1, 2, 3, 4, and 5, and then outputs each value in the array to the console.
+int[] Array = new int[] { 1,2,3,4,5};
+
+foreach (int i in Array)
+{
+    Console.Write( i);
+}
