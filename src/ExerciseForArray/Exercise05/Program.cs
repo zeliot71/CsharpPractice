e951fd2acddcbf9cb0856e315215235e79e3f21f
AliyenSha <zihadshai701@gmail.com>
2023-05-04 10:56:55 +0600

    Exercise 5 codes

---
+++ src/ExerciseForArray/Exercise05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Write a program that creates an array of integers with the values 1, 2, 3, 4, and 5, and then calculates the average of all the values in the array and outputs the result to the console.
+
+int[] nums = new int[] {1,2,3,4,5};
+var sum = 0;
+for(int i = 0; i < nums.Length; i++)
+{
+    sum+= nums[i];
+}
+int average = sum/nums.Length;
+Console.WriteLine(average);
