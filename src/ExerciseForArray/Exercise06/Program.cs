964f730441973a309ef21a31f5b3ec4c3ba8f29f
AliyenSha <zihadshai701@gmail.com>
2023-05-04 21:33:18 +0600

    Exercise 6 codes

--- src/ExerciseForArray/Exercise06/Program.cs
+++ src/ExerciseForArray/Exercise06/Program.cs
@@ -1,9 +1,20 @@
 ﻿// Write a program that creates an array of integers with the values 1, 2, 3, 4, and 5, and then sorts the array in ascending order and outputs the sorted array to the console.
 int[] nums = new int[] {4,5,3,1,2};
-for (int i = 0;i < nums.Length; i++)
+int i,j;
+for (i = 0;i < nums.Length - 1; i++)
 {
-    if (i > nums[0]+1)
+    for(j = 0; j< nums.Length-i-1; j++)
     {
-        Console.WriteLine(i);
+        if (nums[j] > nums[j + 1])
+        {
+            var temp = nums[j];
+
+            nums[j] = nums[j + 1];
+            nums[j + 1] = temp;
+        }
     }
+}
+foreach(int n in nums)
+{
+    Console.Write(n + " ");
 }
