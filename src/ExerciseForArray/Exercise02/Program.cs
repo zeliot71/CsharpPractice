58cb3a10ddd367dd06a4ead8d83bad2bd152389f
AliyenSha <zihadshai701@gmail.com>
2023-05-04 09:39:01 +0600

    Exercise 2 Codes

---
+++ src/ExerciseForArray/Exercise02/Program.cs
@@ -0,0 +1,4 @@
+﻿// Write a program that creates an array of strings with the values "apple", "banana", "cherry", "date", and "elderberry", and then outputs each value in the array to the console.
+string[] Fruits = new string[] { "apple", "Banana", "Cherry", "Date", "Elderberry" };
+foreach(var f in Fruits)
+    Console.WriteLine(f);
