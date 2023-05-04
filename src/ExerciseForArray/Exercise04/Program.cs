5a4f23b65fab3ca67a0ff17b8a832a74fda238d1
AliyenSha <zihadshai701@gmail.com>
2023-05-04 10:49:14 +0600

    Exercise 4 codes

---
+++ src/ExerciseForArray/Exercise04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Write a program that creates an array of integers with the values 1, 2, 3, 4, and 5, and then calculates the sum of all the values in the array and outputs the result to the console.
+
+int[] nums = new int[] { 1, 2, 3, 4, 5 };
+var sum = 0;
+for (int i = 0;i < nums.Length; i++)
+{
+    sum += nums[i];
+}
+Console.WriteLine(sum);
