7cfacbaf5798ec2e73a29233ef80a8aeaa1d4e60
AliyenSha <zihadshai701@gmail.com>
2023-05-05 18:00:31 +0600

    Exercise 7 repeated again to practice using foreach loop

---
+++ src/ExerciseForArray/Exercise07.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// exercise 07 repeate again for using foreach loop
+
+using System.Security.Authentication.ExtendedProtection;
+
+int[][] arrays = new int[2][];
+arrays[0] = new int[3] {1,2,3};
+arrays[1] = new int[3] {4,2,1};
+
+var result = 0;
+foreach (int[] arrayElement in arrays)
+{
+    foreach(int element in arrayElement)
+    {
+        result += element;
+    }
+}
+Console.WriteLine(result);
--- src/ExerciseForArray/Exercise07/Program.cs
+++ src/ExerciseForArray/Exercise07/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Write a program that creates two arrays of integers with the same length, and then calculates the sum of each corresponding element in the arrays and outputs the result to the console.
 int[][] arrays = new int[2][];
-arrays[0]  = new int[3];
-arrays[1]  = new int[3];
+arrays[0] = new int[3];
+arrays[1] = new int[3];
 
 arrays[0][0] = 1;
 arrays[0][1] = 2;
@@ -10,13 +10,13 @@ arrays[1][0] = 1;
 arrays[1][1] = 2;
 arrays[1][2] = 3;
 var result = 0;
-for(int i = 0; i < arrays.Length; i++)
+for (int i = 0; i < arrays.Length; i++)
 {
     int[] arrayElements = arrays[i];
-    for(int j = 0; j< arrayElements.Length; j++)
+    for (int j = 0; j < arrayElements.Length; j++)
     {
         result += arrayElements[j];
-        
+
     }
 }
 Console.WriteLine(result);
