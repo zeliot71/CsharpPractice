4960a8bbd78f62f2c288a8ab50e9c2dff5849af9
AliyenSha <zihadshai701@gmail.com>
2023-05-05 18:16:18 +0600

    Exercise 8 codes

---
+++ src/ExerciseForArray/Exercise08/Program.cs
@@ -0,0 +1,8 @@
+﻿//Write a program that creates an array of integers with the values 1, 2, 3, 4, and 5, and then reverses the order of the values in the array and outputs the reversed array to the console.
+int[] nums = new int[] {1,2,3,4,5};
+
+Array.Reverse(nums);
+foreach(int num in nums)
+{
+    Console.WriteLine(num);
+}
