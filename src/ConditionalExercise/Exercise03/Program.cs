7e6dd8451a8fcbbefe5cf478c9c5373ca83654af
AliyenSha <zihadshai701@gmail.com>
2023-05-06 22:55:56 +0600

    Exercise 3 codes

---
+++ src/ConditionalExercise/Exercise03/Program.cs
@@ -0,0 +1,12 @@
+﻿//Write a program that asks the user to enter a password. If the password is "password123", the program should print "Access granted"; otherwise, it should print "Access denied".
+string pass = Console.ReadLine();
+bool equalPass = pass.Equals("password123");
+
+if (equalPass == true)
+{
+    Console.WriteLine("Access granted");
+}
+else
+{
+    Console.WriteLine("Access denied");
+}
