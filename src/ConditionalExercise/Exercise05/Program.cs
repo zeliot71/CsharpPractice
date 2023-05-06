927ad67da6df42f796a845f9024e731e2ca25144
AliyenSha <zihadshai701@gmail.com>
2023-05-06 23:39:28 +0600

    Exercise 5 codes, 4 is empty

---
+++ src/ConditionalExercise/Exercise05/Program.cs
@@ -0,0 +1,25 @@
+﻿//Write a program that asks the user to enter a letter and then prints out whether the letter is a vowel or a consonant.
+Console.WriteLine("Enter any letter: ");
+string Letter = Console.ReadLine().ToLower();
+
+if (Letter.Length == 1)
+{
+    char letter = Letter[0];
+    switch (Letter)
+    {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            Console.WriteLine("This is a vowel");
+            break;
+        default:
+            Console.WriteLine("This is a consonant!");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Please enter only one letter!");
+}
