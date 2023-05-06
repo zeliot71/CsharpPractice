f57203964033204b7d1834c0e7f9a8305ebdbdaa
AliyenSha <zihadshai701@gmail.com>
2023-05-06 20:03:21 +0600

    Exercise 1 codes

---
+++ src/ConditionalExercise/Exercise01/Program.cs
@@ -0,0 +1,10 @@
+﻿//Write a program that asks the user to enter their age and then prints out "You are an adult" if the user is over 18, or "You are a minor" if they are 18 or younger.
+int age = int.Parse(Console.ReadLine());
+if(age > 18)
+{
+    Console.WriteLine("You're an adult!");
+}
+else if(age<=18)
+{
+    Console.WriteLine("You're a minor!");
+}
