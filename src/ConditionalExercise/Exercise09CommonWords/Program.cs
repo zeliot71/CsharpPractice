b35d97ba324f3b27459d5424ed2cbfe28873def7
AliyenSha <zihadshai701@gmail.com>
2023-05-08 21:33:25 +0600

    unsolved porblem

---
+++ src/ConditionalExercise/Exercise09CommonWords/Program.cs
@@ -0,0 +1,25 @@
+﻿//Write a program that asks the user to enter a sentence and then determines the most common word in the sentence. Ignore capitalization and punctuation.
+using System.Linq.Expressions;
+
+string sentence = Console.ReadLine().ToLower();
+
+string cleanSentence = "";
+foreach (char c in sentence)
+{
+    if (char.IsLetter(c) || c == ' ')
+    {
+        cleanSentence += c;
+    }
+}
+
+string[] newSentence = cleanSentence.Split(' ');
+for(int i = 0;i < newSentence.Length; i++)
+{
+    for(int j = 0; j< newSentence.Length-1; j++)
+    {
+        if (newSentence[i] == newSentence[j])
+            Console.WriteLine(newSentence[j]);
+        break;
+
+    }
+}
