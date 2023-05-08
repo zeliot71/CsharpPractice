0be1ed53cc35e2fc3c65988d3bc300ba1322597f
AliyenSha <zihadshai701@gmail.com>
2023-05-08 09:13:32 +0600

    Exercise06 codes

---
+++ src/ConditionalExercise/Exercise06/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a program that asks the user to enter a year and then prints out whether the year is a leap year or not. Hint: A leap year is a year that is divisible by 4, except for years that are divisible by 100 but not by 400.
+
+int Year = int.Parse(Console.ReadLine());
+
+
+if(Year % 4 == 0 && Year % 100 != 0)
+{
+    Console.WriteLine("Leap Year!");
+}
+else if(Year % 400 == 0)
+{
+    Console.WriteLine("Leap Year!");
+}
+else
+{
+    Console.WriteLine("Not Leap Year!");
+}
