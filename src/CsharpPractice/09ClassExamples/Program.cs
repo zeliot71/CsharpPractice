3165a2165ad4a7f4dd6704a047657adeba039bdf
AliyenSha <zihadshai701@gmail.com>
2023-05-20 19:47:09 +0600

    basics about class,variables,access modifier,methd,constructor and property

---
+++ src/CsharpPractice/09ClassExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _09ClassExamples;
+
+WaterBottle bottle1 = new WaterBottle(250, "Red");
+bottle1.addWater(250);
+
+WaterBottle bottle2 = new WaterBottle(500,"Blue");
+bottle2.addWater(500);
+
+
+Console.WriteLine(bottle2.Capacity);
+Console.WriteLine(bottle1.Color);
---
+++ src/CsharpPractice/09ClassExamples/WaterBottle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _09ClassExamples
+{
+    class WaterBottle
+    {
+        private double _capacity;
+        string _color;
+        private double waterAmount;
+
+        //property
+        public double Capacity 
+        {
+
+            get
+            {
+                if (Capacity < 0)
+                    return 100;
+                else
+                    return Capacity;
+            }
+            set
+            {
+                if(_capacity< 6000)
+                {
+                    _capacity = value;
+                }
+            }
+        }
+        public string Color { get; set; } 
+
+        //constructor
+        public WaterBottle(double capacity, string color)
+        {
+            _capacity = capacity;
+            _color = color;
+        }
+
+        //Method
+        public void addWater(int amount)
+        {
+            waterAmount += amount;
+        }
+    }
+}
