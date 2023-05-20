315fe42109d1b3bd60c4c801870793c221cac354
AliyenSha <zihadshai701@gmail.com>
2023-05-20 22:44:48 +0600

    Exercise04 codes

---
+++ src/ExerciseClasses/Exercise04/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise04;
+
+Student student = new Student("Zihad");
+
+student.addGrade(100);
+student.addGrade(80);
+student.addGrade(50);
+student.addGrade(10);
+
+double avarageGrade = student.CalculateAvarageGrade();
+Console.WriteLine($"{student.Name}'s avarage grade is: {avarageGrade}");
+
+
---
+++ src/ExerciseClasses/Exercise04/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04
+{
+    public class Student
+    {
+        public string Name { get;}
+        public List<int> Grades { get;}
+        
+        public Student(string name) 
+        { 
+            Name = name;
+            Grades = new List<int>();
+        }
+
+        public void addGrade (int grade)
+        {
+            Grades.Add(grade);   
+        }
+        public double CalculateAvarageGrade()
+        {
+            if (Grades.Count == 0)
+            {
+                return 0;
+            }
+            int sum = 0;
+            foreach (int grade in Grades)
+            {
+                sum += grade;
+            }
+            return (double)sum / Grades.Count;
+        }
+    }
+}
