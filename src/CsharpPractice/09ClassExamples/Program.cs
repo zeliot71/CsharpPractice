841b7bb61246d6fa7e8d2053bd034fc9468b570e
AliyenSha <zihadshai701@gmail.com>
2023-05-23 08:53:07 +0600

    repeated again

--- src/CsharpPractice/09ClassExamples/Program.cs
+++ src/CsharpPractice/09ClassExamples/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _09ClassExamples;
 
-WaterBottle bottle1 = new WaterBottle(250, "Red");
+WaterBottle bottle1 = new WaterBottle();
 bottle1.addWater(250);
 
-WaterBottle bottle2 = new WaterBottle(500,"Blue");
+WaterBottle bottle2 = new WaterBottle();
 bottle2.addWater(500);
 
 
--- src/CsharpPractice/09ClassExamples/WaterBottle.cs
+++ src/CsharpPractice/09ClassExamples/WaterBottle.cs
@@ -32,19 +32,33 @@ namespace _09ClassExamples
                 }
             }
         }
-        public string Color { get; set; } 
+        public string Color { get; set; }
 
-        //constructor
-        public WaterBottle(double capacity, string color)
-        {
-            _capacity = capacity;
-            _color = color;
-        }
-
-        //Method
+        //Method & Method Overloading
+        //C# e method er khetre 2 ta kotha mone rakhben (1) obosshoi return type thakte hobe (2) method ekbar call korben r bar bar call kore use korben
         public void addWater(int amount)
         {
             waterAmount += amount;
+        }
+        //method overloading
+        public void addWater(double amount)
+        {
+            waterAmount += amount;
+        }
+
+        //Constructor & Constructor Overloading
+        //constructor er khetre 2 ta kotha mone rakhben, 1- class er name hobe 2- apni constructor na likhleo ekta default vabe thake
+
+        public WaterBottle()
+        {
+            Capacity= 100;
+            Color = "red";
+        }
+        //overloading- er khetre 2 ta kotha mone rakhben: (1) overloading er maddhome multiple constructor lekha jai (2) obosshoi constructor gular paramiter defferent hote hobe
+        public WaterBottle(double capacity, string color)
+        {
+            Capacity= capacity;
+            Color= color;
         }
     }
 }
