12eaa076badfab48a07a015d8626f36fd6e88c4b
AliyenSha <zihadshai701@gmail.com>
2023-05-23 09:31:50 +0600

    method overloading exercise 1 codes

---
+++ src/Overloading/MethodOverloadingExample01/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExample01
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Add(int x,int y, int z)
+        {
+            return x + y + z;
+             
+        }
+        public double Add(double x, double y) 
+        {
+            return x + y;
+        }
+        public int Add(int[] x)
+        {
+            int sum = 0;
+            foreach (int i in x)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ src/Overloading/MethodOverloadingExample01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MethodOverloadingExample01;
+
+Calculator calculator = new Calculator();
+int sum1 = calculator.Add(5,3);
+int sum2 = calculator.Add(5,3,2);
+double sum3 = calculator.Add(5.84665, 3.384651);
+int sum4 = calculator.Add(new int[] { 1, 2, 3, 4, 5, 6 });
+
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
+Console.WriteLine(sum4);
