de8b46cb4d1979e3790750c746927edc1d5bcf00
AliyenSha <zihadshai701@gmail.com>
2023-05-25 00:35:54 +0600

    one more method overloading practice

---
+++ src/Overloading/MethodOverloadExercise04/ArrayUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise04
+{
+    public class ArrayUtils
+    {
+        public int Sum(int[] array)
+        {
+            int sum = 0;
+            foreach (int item in array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        public double Sum(double[] array)
+        {
+            double sum1 = 0;
+            foreach(double i in array)
+            {
+                sum1 += i;
+            }
+            return sum1;
+        }
+    }
+}
---
+++ src/Overloading/MethodOverloadExercise04/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MethodOverloadingExercise04;
+
+ArrayUtils arrayUtils = new ArrayUtils();
+int sum1 = arrayUtils.Sum(new int[] {1,2,3,1,4,4,5,4,7,9});
+double sum2 = arrayUtils.Sum(new double[] { 1.00,2.550,0.55});
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
