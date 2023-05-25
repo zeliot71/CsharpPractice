30e5c031a342420b90103ea3d0e49528247d2989
AliyenSha <zihadshai701@gmail.com>
2023-05-25 11:18:07 +0600

    constructor chainning,readonlyI& const variable  and destructor added

--- src/CsharpPractice/09ClassExamples/WaterBottle.cs
+++ src/CsharpPractice/09ClassExamples/WaterBottle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,20 @@ namespace _09ClassExamples
         private double _capacity;
         string _color;
         private double waterAmount;
+        private readonly string _material= "Plastic";
+        //readonly variable can only declare while writing it and into the constructor
+        private const string deameter = "3.1416";
+        //constant variable only likhar somoy declare kora jai
+        private List<string> _icons = new List<string>();
+        //collection er memory destroy korar jonno ache Destructor or Finalizer
+
+        ~WaterBottle()
+        {
+            _icons.Clear();
+            _icons = null;
+            //Ey sei bikkhato destructor ja khub kom use kora hoi c# e
+            //karon default vabe ekta gurbage collector thake ja memory clear kore  
+        }
 
         //property
         public double Capacity 
@@ -43,18 +58,20 @@ namespace _09ClassExamples
         //method overloading
         public void addWater(double amount)
         {
+            addWater(amount);
+            //we can call any method into another method.
             waterAmount += amount;
         }
 
         //Constructor & Constructor Overloading
         //constructor er khetre 2 ta kotha mone rakhben, 1- class er name hobe 2- apni constructor na likhleo ekta default vabe thake
-
-        public WaterBottle()
+        //we can't call a constructor into another constractor so that constructor chainning came
+        public WaterBottle() : this(200, "green")
         {
             Capacity= 100;
             Color = "red";
         }
-        //overloading- er khetre 2 ta kotha mone rakhben: (1) overloading er maddhome multiple constructor lekha jai (2) obosshoi constructor gular paramiter defferent hote hobe
+        //overloading- er khetre 2 ta kotha mone rakhben: (1) overloading er maddhome multiple constructor same name lekha jai (2) obosshoi constructor gular paramiter defferent hote hobe
         public WaterBottle(double capacity, string color)
         {
             Capacity= capacity;
