755e80ca68b6e97022686280d49103a989edea21
AliyenSha <zihadshai701@gmail.com>
2023-05-28 09:50:11 +0600

    program cs file

--- src/CsharpPractice/12ExampleOFstatic/Program.cs
+++ src/CsharpPractice/12ExampleOFstatic/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using _12ExampleOFstatic;
+//Static somporke aro duita kotha-
+//(1) Er instance create korte hoina direct call kora jai
+//(2) static puro class er jonno 1 pcs e likha jabe & er moddhe instance create kore multiple person create kora jaina
+EmailUtility.count = 2;
+EmailUtility.Subject = "greetings";
+EmailUtility.SendEmail("Zihad Sahriar", "zihad.sn@myself.com", "Hello", "Welcome to c# world");
+
