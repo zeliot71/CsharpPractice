79f4874446be43b09f2e84f181498507d5336405
AliyenSha <zihadshai701@gmail.com>
2023-05-30 01:23:42 +0600

    A Bit++ problem solve

---
+++ src/ProblemSolving/aBit++/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+var X = 0;
+for (int i = 1; i <= n; i++)
+{
+    string input = Console.ReadLine();
+    if (input.Contains('+'))
+    {
+        X++;
+    }
+    else if (input.Contains('-'))
+    {
+        X--;
+    }
+   
+}
+Console.WriteLine(X);
