6cfec8ee6af4fcf68fde6b025dc0f042915d203f
AliyenSha <zihadshai701@gmail.com>
2023-05-29 08:01:19 +0600

    Simple dharona about abstract class

--- src/CsharpPractice/12ExampleOFstatic/EmailUtility.cs
+++ src/CsharpPractice/12ExampleOFstatic/EmailUtility.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace _12ExampleOFstatic
 {
-    internal class EmailUtility
+    public static class EmailUtility
     {
+        //Static er khetre duita kotha mone rakhte hobe-
+        //(1) static class er vitor feild,property,variables, constructor and methods sob static hote hobe
+        //(2) Non static class er vitor o static variables likha jai
+
+        public static int count;
+        //static variable
+        public static string Subject { get; set; }
+        //static property
+
+        public static void SendEmail(string name, string email,string subject,string body)
+        {
+        //static method
+            
+        }
+
+        static EmailUtility()
+        {
+        //static constructor er khetre access modifier use kora jaina
+
+        }
+
+
     }
 }
