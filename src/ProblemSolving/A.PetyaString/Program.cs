0579587560ae57036b59834ddab79f8f148f336f
AliyenSha <zihadshai701@gmail.com>
2023-05-30 21:51:04 +0600

    A Petya and String Completed

---
+++ src/ProblemSolving/A.PetyaString/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string str1 = Console.ReadLine().ToLower();
+string str2 = Console.ReadLine().ToLower();
+var compaRes = str1.CompareTo(str2);
+
+if (compaRes > 0)
+{
+    Console.WriteLine(1);
+}
+else if(compaRes == 0)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(-1);
+}
+
