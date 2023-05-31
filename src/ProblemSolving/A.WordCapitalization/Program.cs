dfd27a3b0ae2a4e4c07c839d8b4332fd3dec533c
AliyenSha <zihadshai701@gmail.com>
2023-05-31 21:49:25 +0600

    a.WordCapitalization solved without any help

---
+++ src/ProblemSolving/A.WordCapitalization/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input =Console.ReadLine();
+string firstLetter = input.Substring(0,1).ToUpper();
+
+string result =firstLetter+ input.Remove(0,1);
+Console.WriteLine(result);
