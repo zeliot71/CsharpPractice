aee72a562680c596179994517b731072b66786a0
AliyenSha <zihadshai701@gmail.com>
2023-05-31 21:48:41 +0600

    One more problem solved without any help

---
+++ src/ProblemSolving/A.HelpfulMaths/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] s = Console.ReadLine().Split('+');
+int[] newS = new int[s.Length];
+
+for(int i = 0; i < s.Length; i++)
+{
+    newS[i] = int.Parse(s[i]);
+   
+}
+Array.Sort(newS);
+string resulut = string.Join("+", newS);
+Console.WriteLine(resulut);
