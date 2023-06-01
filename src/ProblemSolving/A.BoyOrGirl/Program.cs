caf810bd26c5d8a22a76df8e4477b4220373bcb8
AliyenSha <zihadshai701@gmail.com>
2023-06-02 01:27:13 +0600

    Another problem solved

---
+++ src/ProblemSolving/A.BoyOrGirl/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+string newStr = new string(input.Distinct().ToArray());
+
+int gender = newStr.Length;
+
+if (gender % 2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+{
+    Console.WriteLine("IGNORE HIM!");
+}
