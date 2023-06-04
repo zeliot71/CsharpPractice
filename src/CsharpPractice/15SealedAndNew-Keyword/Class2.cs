b4671f48ec4ba422912fe8434ab1047428a96051
AliyenSha <zihadshai701@gmail.com>
2023-06-04 08:26:39 +0600

    Sealed and new keyword

---
+++ src/CsharpPractice/15SealedAndNew-Keyword/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15SealedAndNew_Keyword
+{
+    internal class Class2
+    {
+    }
+}
