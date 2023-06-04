223d6c879bdba1a3fd63286787ba6132dc1444c2
AliyenSha <zihadshai701@gmail.com>
2023-06-04 08:09:37 +0600

    few more work remain undone

---
+++ src/ExerciseInterface/Exercise01/ConsoleLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public class ConsoleLogger : ILogger
+    {
+        void ILogger.LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILogger.LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ExerciseInterface/Exercise01/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public interface ILogger
+    {
+        public void LogInfo(string message);
+        public void LogError(string message);
+    }
+}
