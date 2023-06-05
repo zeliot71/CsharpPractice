7e26bf297fbb5719e01644a57d08613476b48fae
AliyenSha <zihadshai701@gmail.com>
2023-06-05 10:22:15 +0600

    task 3 completed

---
+++ src/Exam2/Task3/RandomNumberGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class RandomNumberGenerator
+    {
+        private static int _seed;
+        private static Random _rand;
+
+        static RandomNumberGenerator()
+        {
+            _seed = DateTime.Now.Microsecond;
+            _rand = new Random(_seed);
+        }
+
+        public static int Generate()
+        {
+            return _rand.Next();
+        }
+    }
+}
