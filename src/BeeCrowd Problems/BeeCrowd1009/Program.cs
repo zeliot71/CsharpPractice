6bdd39bc4b8d69c6fb0a2f01ceacbaf654d58de0
AliyenSha <zihadshai701@gmail.com>
2023-06-05 21:54:49 +0600

    Problem1009

---
+++ src/BeeCrowd Problems/BeeCrowd1009/Program.cs
@@ -0,0 +1,7 @@
+﻿//==== BeeCrowd 1009 ====>>
+string name = Console.ReadLine();
+double salary = double.Parse(Console.ReadLine());
+double sale = double.Parse(Console.ReadLine());
+double total = (sale*0.15)+salary;
+Console.WriteLine("TOTAL = R$ "+ total.ToString("0.00"));
+
