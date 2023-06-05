cb30df10a5165be2a7143c025545ea05107e6adf
AliyenSha <zihadshai701@gmail.com>
2023-06-05 21:54:09 +0600

    Problem1007

---
+++ src/BeeCrowd Problems/BeeCrowd1007/Program.cs
@@ -0,0 +1,8 @@
+﻿//===== BeeCrowd Problem 1007 ===>
+int a, b, c, d;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+d = int.Parse(Console.ReadLine());
+var result = (a*b)-(c*d);
+Console.WriteLine($"DEFERENCA = {result}"); 
