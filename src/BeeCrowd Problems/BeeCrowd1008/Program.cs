d4386e17cb5250ca2e6a3e46408517cb3c02ce0f
AliyenSha <zihadshai701@gmail.com>
2023-06-05 21:54:33 +0600

    Problem1008

---
+++ src/BeeCrowd Problems/BeeCrowd1008/Program.cs
@@ -0,0 +1,14 @@
+﻿//==== BeeCrowd 1008 ====>>
+//p for PAYMENT,h for HOUR,n for ID NUMBER && tp for TOTAL PAYMENT
+
+double n, h,p,tp;
+Console.WriteLine("Enter Number");
+n = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter your payment per hour");
+p = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter total hour your worked in month");
+h = double.Parse(Console.ReadLine());
+tp = p * h;
+
+Console.WriteLine("NUMBER = "+ n);
+Console.WriteLine("SALARY = U$ "+ tp);
