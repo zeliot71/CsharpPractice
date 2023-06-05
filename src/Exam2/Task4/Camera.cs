7c2f6d3076365ec64749de334d5000157cade1a5
AliyenSha <zihadshai701@gmail.com>
2023-06-05 10:41:12 +0600

    Task 4 done

---
+++ src/Exam2/Task4/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Cannon
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void TakePhoto(int width, int height)
+        {
+            Console.WriteLine($"Took photo of size: {width}x{height}");
+        }
+
+        public Cannon(string model,string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public Cannon() : this("Cannon 505D","Black")
+        {
+            Model = "GoPro701z";
+            Color = "Gray";
+        }
+
+    }
+}
