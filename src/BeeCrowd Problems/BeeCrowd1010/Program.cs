3340f3d9635e74fe13026eb0c574f66c340a0407
AliyenSha <zihadshai701@gmail.com>
2023-06-05 21:55:07 +0600

    Problem1010

---
+++ src/BeeCrowd Problems/BeeCrowd1010/Program.cs
@@ -0,0 +1,10 @@
+﻿//==== BeeCrwod 1010===>>
+int id1, id2, qt1, qt2;
+double  price1,price2;
+string[] input1 = Console.ReadLine().Split(' ');
+string[] input2 = Console.ReadLine().Split(' ');
+id1 = int.Parse(input1[0]);
+id2 = int.Parse(input2[0]);
+price1 = int.Parse(input1[1]);
+price2 = int.Parse(input2[1]);
+Console.WriteLine("Hello, World!");
