223def9e5865c36a356eb157dee2a6343494735d
AliyenSha <zihadshai701@gmail.com>
2023-06-05 10:11:28 +0600

    Task1 completed

---
+++ src/Exam2/Task1/AsusTUF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class AsusTUF : Machine
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Machine starting new way!");
+        }
+    }
+}
---
+++ src/Exam2/Task1/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Machine
+    {
+        public virtual void Start()
+        {
+            Console.WriteLine("Machine Started");
+        }
+    }
+}
