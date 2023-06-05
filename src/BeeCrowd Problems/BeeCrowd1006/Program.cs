49618f530a2b88ad1ec6795b4e6c934759ea0d75
AliyenSha <zihadshai701@gmail.com>
2023-06-05 21:52:59 +0600

    problem1006

---
+++ src/BeeCrowd Problems/BeeCrowd1006/Program.cs
@@ -0,0 +1,8 @@
+﻿//==== BeeCrowd 1005 ====>
+
+
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+
+double media = (a/11 * 3.5) + (b/11 * 7.5);
+Console.WriteLine("MEDIA = " + media.ToString("0.00000"));
