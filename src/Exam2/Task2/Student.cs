6aaf195fc086944902c210aa77e7ee7b14aa2a6f
AliyenSha <zihadshai701@gmail.com>
2023-06-05 10:19:01 +0600

    task 2 done

---
+++ src/Exam2/Task2/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Student : Institute
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string GenerateId()
+        {
+            return "S" + Name;
+        }
+
+        public override void ProgramingClub()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Exam2/Task2/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Teacher : Institute
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth {get;set;}
+
+        public string GenerateId()
+        {
+            return "T"+ Name;
+        }
+
+        public override void ProgramingClub()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
