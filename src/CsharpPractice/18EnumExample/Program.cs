945a411060a4fc48e196b76c016779de923a34ed
AliyenSha <zihadshai701@gmail.com>
2023-06-06 11:31:00 +0600

    Enum concepts more about to practice need

---
+++ src/CsharpPractice/18EnumExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _18EnumExample;
+
+User user = new User();
+user.Name = "Test";
+int status = (int)UserStatus.Active;
+Console.WriteLine(status);
+string serial = UserStatus.Verified.ToString();
+Console.WriteLine(serial);
+
+
+
+char c = (char)Gender.Male;
+Console.WriteLine(c);
---
+++ src/CsharpPractice/18EnumExample/UserStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18EnumExample
+{
+    public enum UserStatus
+    {
+        Verified ,
+        Active,
+        Blocked,
+        Inactive,
+        Deleted
+            //Specipic vibinno options status hisebe neya hoyeche
+    }
+    //Onek somoi kono kichur status number & textualy bojhanor  proyojon hoy tokhon enum use kora subidhar
+
+    public enum Gender 
+    {
+        Male = 'M',
+        Female = 'F',
+        Other = 'O'
+
+    }
+
+}
