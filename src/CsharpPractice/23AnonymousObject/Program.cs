a54020d61102f7dc3f8a1b5183a427169bc55a83
AliyenSha <zihadshai701@gmail.com>
2023-06-12 20:56:24 +0600

    Anonymous object

---
+++ src/CsharpPractice/23AnonymousObject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Anonymous object; temon use hoina & eta reference type
+dynamic o = new { Name = "Zihad", Age = 22 };
+
+//Console.WriteLine(o.Name);
+//Sorasori object'k call kora jaina so that we should use Dynamic or reflection instead of object
+//Dynamic keyword use korle intelligence deina error hole kintu compile korar somoy erorr dei thikoi
+Console.WriteLine(o.Name);
