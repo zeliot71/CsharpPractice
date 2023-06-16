7eb4220b3d2b037c3ee32861992fb81f4781a9b6
AliyenSha <zihadshai701@gmail.com>
2023-06-16 10:50:42 +0600

    done but not submitted yet

---
+++ src/ProblemSolving/A.StringTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+string input = Console.ReadLine().ToLower();
+
+
+StringBuilder sb = new StringBuilder();
+foreach (char c in input)
+{
+    if ("aeiouy".Contains(c))
+    {
+        sb.Replace(c.ToString(),"");
+    }
+    else
+    {
+        sb.Append('.').Append(c);
+        
+    }
+   
+}
+
+string result = sb.ToString();
+Console.Write(result);
