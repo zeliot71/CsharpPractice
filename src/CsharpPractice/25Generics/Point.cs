a51985e4534b8c6d7bd66b40b861c583e74f508b
AliyenSha <zihadshai701@gmail.com>
2023-06-16 09:30:42 +0600

    Generic parameter & Generic Constrain

---
+++ src/CsharpPractice/25Generics/Point.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25Generics
+{
+    public class Point <T,V,M> 
+        where T: class
+        where V : struct //They are constrain 
+        where M: IDisposable
+    {//ekhane just placeholder ta likha hoise & caile bar bar vinno vinno data type er jonno eta use kora jabe
+        //Ey type T ta only er vitorei kaj korbe
+        //generic variables
+        public T m;
+        public V m1; 
+        public V m2;
+
+        //generic properties
+        public T X { get; set; } 
+        public T Y { get; set; }
+        public T Z { get; set; }
+        
+
+        //Generic method 
+        public virtual T Sum(T point)
+        {//return type & variables er age data type likha jai
+            throw new NotImplementedException();
+        }
+
+        //generic constructor
+        public Point (T Oldpoint) //parameter 
+        {
+            //constructor e <> angle bracet use kora lagena
+
+        }
+
+        public V Multiply<U>(U a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    //jokhon kono datar type poristhitir sapekkhe change kora pryojon segulu age thekei class e fixed kore na diye
+    // ami segulo ke use korar somoy define korbo
+
+
+    //override
+    public class advancePoint<R, W, Y> : Point<W, Y, R>
+    {
+        public advancePoint(W Oldpoint) : base(Oldpoint)
+        {
+        }
+
+        public override W Sum(W point)
+        {
+            return base.Sum(point);
+        }
+    }
+
+}
---
+++ src/CsharpPractice/25Generics/Program.cs
@@ -0,0 +1,11 @@
+﻿// Class 14 Generic Parameter [24:52 - 1: 11: 00]
+// Generic Constrain [1:12:00 - ]
+//Generics: generics allow you to define reusable code that can work with different types of data.
+//They provide a way to create classes, structures, methods, and interfaces that can work with multiple data types without sacrificing type safety.
+
+using _25Generics;
+using System.Data.SqlClient;
+
+Point <string, double, SqlConnection> point= new Point<string,double,FileStream>(2);
+
+
