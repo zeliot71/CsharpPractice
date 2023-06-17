0829b35c377a7d86b1c8aa3b2bd163cd46660ded
AliyenSha <zihadshai701@gmail.com>
2023-06-17 19:41:16 +0600

    two more problem solved

---
+++ src/ProblemSolving/A.StoneOnTable/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+string stones = Console.ReadLine();
+
+int count = 0;
+for (int i = 1; i < n; i++)
+{
+    if (stones[i] == stones[i - 1])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
--- src/ProblemSolving/A.StringTask/Program.cs
+++ src/ProblemSolving/A.StringTask/Program.cs
@@ -2,20 +2,18 @@
 
 string input = Console.ReadLine().ToLower();
 
-
 StringBuilder sb = new StringBuilder();
 foreach (char c in input)
 {
     if ("aeiouy".Contains(c))
     {
-        sb.Replace(c.ToString(),"");
+        sb.Replace(c.ToString(), "");
     }
     else
     {
         sb.Append('.').Append(c);
-        
+
     }
-   
 }
 
 string result = sb.ToString();
