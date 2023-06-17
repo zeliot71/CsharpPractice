32e54cca7250ca0ec69d5a08d645b72cbd670d40
AliyenSha <zihadshai701@gmail.com>
2023-06-18 04:10:27 +0600

    two bear solved

---
+++ src/ProblemSolving/A.BearAndBigBrother/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] weights = Console.ReadLine().Split(' ');
+
+int a = int.Parse(weights[0]);
+int b = int.Parse(weights[1]);
+int Y = 0;
+
+while(a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        Y += 1;
+    }
+
+
+Console.WriteLine(Y);
