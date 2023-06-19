0b130c9a0d9dc7e8b5db5c263ef4d6d76e0e61df
AliyenSha <zihadshai701@gmail.com>
2023-06-19 13:33:15 +0600

    a solder and bananas solved

---
+++ src/ProblemSolving/A.SoldirBananas/Program.cs
@@ -0,0 +1,23 @@
+﻿// k - prices of per pcs banana
+//n - the dollar he has
+//w - the number of banana he want to buy
+
+string[] line = Console.ReadLine().Split(' ');
+int k = int.Parse(line[0]);
+int n = int.Parse(line[1]);
+int w = int.Parse(line[2]);
+
+int TotalK = 0;
+
+for(int i = 1; i <= w; i++)
+{
+    TotalK += k * i;
+}
+if (TotalK < n)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(TotalK - n);
+}
