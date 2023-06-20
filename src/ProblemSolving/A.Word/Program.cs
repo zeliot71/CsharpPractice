e0a41b2b628d60c92bf22d3426bb3129dafe7a14
AliyenSha <zihadshai701@gmail.com>
2023-06-20 19:40:31 +0600

    Annother problem solved

---
+++ src/ProblemSolving/A.Word/Program.cs
@@ -0,0 +1,27 @@
+﻿string word = Console.ReadLine();
+var Lower = 0;
+var Upper = 0;
+
+foreach(var c in word)
+{
+    if (char.IsLower(c))
+    {
+        Lower++;
+    }
+    else if (char.IsUpper(c))
+    {
+        Upper++;
+    }
+}
+if(Upper > Lower)
+{
+    Console.WriteLine(word.ToUpper());
+}
+else if(Lower > Upper)
+{
+    Console.WriteLine(word.ToLower());
+}
+else if(Upper == Lower)
+{
+    Console.WriteLine(word.ToLower());
+}
