5fc3efcbf2aa57ae0afca29e2bdc54a6d915d6f5
AliyenSha <zihadshai701@gmail.com>
2023-06-23 22:18:30 +0600

    all about operator overloading and generic operator overloading

---
+++ src/CsharpPractice/26OperatorOverloading/Fraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26OperatorOverloading
+{
+    public readonly struct Fraction
+    {
+        private readonly int num;
+        private readonly int den;
+
+
+        public Fraction(int numerator, int denominator)
+        {
+            if 
+                (denominator == 0) throw new ArgumentException("Denominator cannot be zero", nameof(denominator));
+            num = numerator;
+            den = denominator;
+        }
+       
+    }
+}
---
+++ src/CsharpPractice/26OperatorOverloading/Program.cs
@@ -0,0 +1,5 @@
+﻿// Class  15 - Start Time [0:11:55 - ]
+//    5     
+//........
+//    7
+//ey dhoroner fracton likhar jonno .Net e built in kono way nai, tai amra fraction value banabo
