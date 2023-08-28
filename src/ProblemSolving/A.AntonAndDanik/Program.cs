41fe4e3fb42f2c099e8745b0cd18bd39d5477670
AliyenSha <zihadshai701@gmail.com>
2023-08-28 12:07:59 +0600

    New problem solved

---
+++ src/ProblemSolving/A.AntonAndDanik/Program.cs
@@ -0,0 +1,23 @@
+﻿//A Anton and Danik
+
+int n = int.Parse(Console.ReadLine());
+string s = Console.ReadLine();
+var Danik = 0;
+var Anton = 0;
+
+for(int i = 0;i < s.Length; i++)
+{
+    if (s[i] == 'A')
+        Anton++;
+    else if(s[i] == 'D')
+        Danik++;
+}
+
+if (Danik > Anton)
+{
+    Console.WriteLine("Danik");
+}
+else if (Anton == Danik) 
+    Console.WriteLine("Friendship");
+else
+    Console.WriteLine("Anton");
