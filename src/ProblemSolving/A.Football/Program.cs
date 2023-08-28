a0799345a2837748f5ce2fb7e8139d9eb0b23993
AliyenSha <zihadshai701@gmail.com>
2023-08-28 12:07:44 +0600

    New problem tried to solve

---
+++ src/ProblemSolving/A.Football/Program.cs
@@ -0,0 +1,17 @@
+﻿//A. Football
+string input = Console.ReadLine();
+
+var danegerous = 0;
+var notDangerous = 0;
+for(int i = 0; i < input.Length; i++)
+{
+    //int m = int.Parse(i);
+    //if(m == 0)
+    //    danegerous++;
+    //else if(m == 1)
+        notDangerous++;
+}
+if (notDangerous >= 7)
+    Console.WriteLine("No");
+else if (danegerous >= 7)
+    Console.WriteLine("Yes");
