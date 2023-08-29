9d72c05297b3e0aa10536331dfec27c3b296c919
AliyenSha <zihadshai701@gmail.com>
2023-08-29 08:57:10 +0600

    Got stuck

---
+++ src/ProblemSolving/A.BeutifullYear/Program.cs
@@ -0,0 +1,20 @@
+﻿//A Beutifull Year
+int Y = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    Y++;
+    string year = Y.ToString();
+    bool hasDistinctDigits = true;
+    for(int i =0; i < year.Length -1; i++)
+    {
+        for(int j =i+1; j < year.Length; j++)
+        {
+            if (year[i] == year[j])
+                hasDistinctDigits = false;
+            break;
+        }
+        
+    }
+    break;
+}
